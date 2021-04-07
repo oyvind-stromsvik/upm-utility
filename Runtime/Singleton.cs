db5d30466813f2be2a22a6dcc24e7a8dae6ebe27
Øyvind Strømsvik <oyvind@nymedia.no>
2021-04-07 20:56:58 +0200

    Add class description

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Singleton class.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class Singleton<T> : MonoBehaviour where T : Component {
 
     public static T Instance { get; private set; }
